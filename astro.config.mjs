690ed9c7ecd84b8f294bb4dfebb9b63637380317
sorozco <sergiorozco.ochoa@gmail.com>
2025-08-16 19:42:45 -0500

    Adding scafold configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import auth from 'auth-astro';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [auth()]
+});
