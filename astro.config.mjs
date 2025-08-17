1c666bf0a86fe9cb9f7b3e4a0c479230df5b4f51
sorozco <sergiorozco.ochoa@gmail.com>
2025-08-16 19:50:15 -0500

    adding netlify adaptar

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import auth from 'auth-astro';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [auth()]
+  integrations: [auth()],
+  adapter: netlify()
 });
